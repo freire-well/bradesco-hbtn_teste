fb2e838b32b39bb7da333ede56e76f372a8e3d3d
Wellington Freire <wellington.freire@live.com>
2025-10-11 10:34:41 -0300

    Fix test

--- 0x01/mei/src/PersonTest.java
+++ 0x01/mei/src/PersonTest.java
@@ -3,8 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonTest {
     Person p;
