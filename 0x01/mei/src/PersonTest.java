1c9ea478a50d564aec7c2c4d274c47f7e44f7621
Wellington Freire <wellington.freire@live.com>
2025-10-11 10:13:06 -0300

    initial Commit

---
+++ 0x01/mei/src/PersonTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PersonTest {
+    Person p;
+   @BeforeEach
+   void setup(){
+        p = new Person("Paul", "McCartney", new Date(2000, 8, 8), true, true,  true);
+
+    }
+
+
+    @Test
+    void show_full_name(){
+       assertEquals("Paul McCartney", p.fullName());
+    }
+
+    @Test
+    void test_calculateYearlySalary(){
+       p.setSalary(1200);
+       assertEquals(14400 ,p.calculateYearlySalary());
+    }
+
+    @Test
+    void person_is_MEI(){
+       p.setSalary(9000);
+       p.setPensioner(false);
+       p.setAnotherCompanyOwner(false);
+       p.setPublicServer(false);
+       assertTrue(p.isMEI());
+    }
+
+    @Test
+    void person_is_not_MEI(){
+       p.setSalary(15000);
+       p.setPensioner(true);
+       p.setAnotherCompanyOwner(true);
+       p.setPublicServer(true);
+       assertTrue(p.isMEI());
+    }
+}
